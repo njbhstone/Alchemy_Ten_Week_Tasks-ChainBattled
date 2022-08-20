b2fe3b1c38d11369604a1a4a7111c399888584db
njbhstone <njbhstone@gmail.com>
2022-08-20 14:25:34 +0800

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: process.env.TESTNET_RPC,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  }
+};
+// 0x504fbF7E3Fb43CB79668105b23CDAD70A7Cd1213
